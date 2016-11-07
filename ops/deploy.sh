7a94dc8da9fc682a082e4294cc1a55605ef28ae9
aliasav <saurabhav.torres@gmail.com>
2016-11-07 18:35:52 +0530

    added deploy script

---
+++ ops/deploy.sh
@@ -0,0 +1,20 @@
+# run this script as app user and virtualenv activated
+
+# fetch code
+cd ../mindValleyTasks/
+git checkout -- static/angular_apps/dashboard/app.js
+git pull origin feature/ttt
+
+# make app.js changes
+source ops/angularjs_settings.sh
+
+# perform collectstatic
+python manage.py collectstatic --noinput
+
+# migrate
+python manage.py migrate
+
+# server restart
+source uwsgi_server_conf/server_stop.sh
+sleep 2
+source uwsgi_server_conf/server_start.sh
