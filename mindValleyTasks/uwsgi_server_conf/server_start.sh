cefc74a7d65424d41d758512d258ea91e428582b
aliasav <saurabhav.torres@gmail.com>
2016-11-07 17:08:21 +0530

    updated uwsgi settings

--- mindValleyTasks/uwsgi_server_conf/server_start.sh
+++ mindValleyTasks/uwsgi_server_conf/server_start.sh
@@ -2,6 +2,6 @@
 
 # !! DONOT run as ROOT !!
 
-uwsgi --uid=1000 --gid=1000 /home/nws/workspace/codebase/
+uwsgi --uid=1000 --gid=1000 /home/aliasav/workspace/MindValleyTasks/mindValleyTasks/uwsgi_server_conf/server.ini
 
 #ini file should be in the same folder from where this script is being run
